f3fd50c6f5ed714ab60b03f3268237b13b30b538
int0p <closer.fz@gmail.com>
2024-04-26 19:46:14 +0900

    diary, habit time

--- src/lib/components/tenMTable/tenMResult.svelte
+++ src/lib/components/tenMTable/tenMResult.svelte
@@ -88,13 +88,13 @@
 					{#if min % 10 === 0}
 						<td class=" !m-0 !border-0 !border-l !p-0 relative" class:colored={cellColors[hour][0][min]}>
 							{#if getHabitIcon(hour, min)}
-								<svelte:component this={getHabitIcon(hour, min)} size="34" class="absolute left-0 -top-0.5  p-1 z-10  bg-zinc-200 rounded-full " />
+								<svelte:component this={getHabitIcon(hour, min)} size="34" class="absolute left-0 -top-0.5  p-0.5 z-10  bg-zinc-200 rounded-full shadow-md border-white border-2 " />
 							{/if}
 						</td>
 					{:else}
 						<td class="!m-0 !border-0 !p-0 relative" class:colored={cellColors[hour][0][min]}>
 							{#if getHabitIcon(hour, min)}
-								<svelte:component this={getHabitIcon(hour, min)} size="34" class="absolute left-0 -top-0.5 p-1 z-10 bg-zinc-200 rounded-full " />
+								<svelte:component this={getHabitIcon(hour, min)} size="34" class="absolute left-0 -top-0.5 p-0.5 z-10 bg-zinc-200 rounded-full shadow-md border-white border-2 " />
 							{/if}
 						</td>
 					{/if}
