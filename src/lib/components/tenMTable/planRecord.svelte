524bb3b347398e1b16ea158dfddb2cf188fa1f68
int0p <closer.fz@gmail.com>
2024-04-29 16:37:30 +0900

    tenM, show proj color

--- src/lib/components/tenMTable/planRecord.svelte
+++ src/lib/components/tenMTable/planRecord.svelte
@@ -7,7 +7,7 @@
 	let settingVisible = false;
 
 	const hours = Array.from({ length: 24 }, (_, i) => i);
-	const columns = [0, 10, 20, 30, 40, 50];
+	const tenMinute = [0, 10, 20, 30, 40, 50];
 	$: currentTimeDisplay = formatTime($currentTime);
 	$: isAM = new Date().getHours() < 12;
 
@@ -16,7 +16,7 @@
 	});
 
 	let cellColors = Array.from({ length: 24 }, () => [
-		Array.from({ length: 6 }, () => ({ colorFill: false, record: null }))
+		Array.from({ length: 6 }, () => ({ colorFill: 0, record: null }))
 	]);
 
 	let projects = [
@@ -27,32 +27,43 @@
 		{ color: '#a78bfa', id: 'p5', title: '프로젝트 5' },
 		{ color: '#818cf8', id: 'p6', title: '프로젝트 6' },
 		{ color: '#60a5fa', id: 'p7', title: '프로젝트 7' },
-		{ color: '#38bdf8', id: 'p8', title: '프로젝트 8' },
+		{ color: '#38bdf8', id: 'p8', title: '프로젝트 8' }
 	];
 
 	let records = [
 		{ start: '12:10', end: '13:30', dragged: true },
-		{ start: '0:30', end: '2:00', projectId: 'p1' },
-		{ start: '4:00', end: '5:30', projectId: 'p2' },		
-		{ start: '9:30', end: '10:15', projectId: 'p3' },
+		{ start: '0:30', end: '2:00', projectId: 'p1' , dragged: true},
+		{ start: '4:00', end: '5:30', projectId: 'p2' , dragged: true},
+		{ start: '9:30', end: '10:20', projectId: 'p3' , dragged: true},
 		{ start: '10:20', end: '11:40', dragged: true },
-		{ start: '22:30', end: '23:45', projectId: 'p8' }
+		{ start: '22:30', end: '23:40', projectId: 'p8' , dragged: true}
 	];
 
-	function getProjectColorPosition(hour,min){
-		let record = records.find(record => {
+	function getStartRecordPosition(hour, min) {
+		let record = records.find((record) => {
 			const startTime = record.start.split(':').map(Number);
-			return startTime[0] === hour && startTime[1] === min;			
+			return startTime[0] === hour && startTime[1] === min;
 		});
 		return record;
 	}
 
+	function getEndRecordPosition(hour, min) {
+		let record = records.find((record) => {
+			const endTime = record.end.split(':').map(Number);
+			if(min===60) {
+				hour += 1;
+				min = 0;
+			}
+			return endTime[0] === hour && endTime[1] === min;			
+		});
+		return record;
+	}
 	function getCellColor() {
 		// map records color
-		records.map((record)=>{
-			const project = projects.find(project => project.id === record.projectId);
-			record.color = project?.color || "#3f3f46";
-		})
+		records.map((record) => {
+			const project = projects.find((project) => project.id === record.projectId);
+			record.color = project?.color || '#3f3f46';
+		});
 		for (let record of records) {
 			let [startHour, startMin] = record.start.split(':').map(Number);
 			let [endHour, endMin] = record.end.split(':').map(Number);
@@ -87,77 +98,80 @@
 	// Dragging
 	let dragging = false;
 	let dragStartHour = null;
-	let dragStartColumn = null;
+	let dragStartMin = null;
 	let dragEndHour = null;
-	let dragEndColumn = null;
+	let dragEndMin = null;
 
 	// Handle the mouse down event
-	function handleMouseDown(hour, column, event, record) {
+	function handleMouseDown(hour, min, event, record) {
+		event.preventDefault();
+		event.stopPropagation();
 		if (record) return;
 		dragging = true;
 		dragStartHour = hour;
-		dragStartColumn = column;
-		dragEndHour = hour;
-		dragEndColumn = column;
+		dragStartMin = min;
+		cellColors[dragStartHour][0][dragStartMin].colorFill = 1;
 		console.log('dragging');
-		event.preventDefault(); // Prevent text selection
-		event.stopPropagation(); // Prevent the event from bubbling up to the table
-	}
-
-	// Handle the mouse up event
-	function handleMouseUp(hour, column) {
-		if (dragging) {
-			// Adjust the end time if dragging ended in the next hour's first column
-			if (column === 0 && hour > dragStartHour) {
-				column = 60;
-				hour--;
-			}
-
-			if (dragStartHour === hour && column - dragStartColumn < 10) {
-				// Reset dragging state
-				dragging = false;
-				dragEndHour = null;
-				dragEndColumn = null;
-				cellColors[hour][0][column / 10].colorFill = 0;
-				return;
-			}
-			const startMinute = dragStartColumn;
-			const endMinute = column === 50 ? 0 : column + 10;
-			const startHour = dragStartHour;
-			const endHour = column === 50 ? hour + 1 : hour;
-
-			// Calculate the duration of the drag in minutes
-			const startTime = startHour * 60 + startMinute;
-			const endTime = endHour * 60 + endMinute;
-
-			if (startTime < endTime) {
-				const newRecord = {
-					start: `${startHour}:${startMinute.toString().padStart(2, '0')}`,
-					end: `${endHour}:${endMinute.toString().padStart(2, '0')}`,
-					dragged: true // Indicate this record was added by dragging
-				};
-				records = [...records, newRecord];
-				cellColors = getCellColor();
-				console.log('Added new record:', newRecord);
-			} else {
-				cellColors[startHour][0][startMinute / 10].colorFill = 0;
-			}
-
-			// Reset dragging state
-			dragging = false;
-			dragEndHour = null;
-			dragEndColumn = null;
-		}
 	}
 
 	// Handle the mouse enter event for real-time feedback
-	function handleMouseMove(hour, columnIdx) {
+	function handleMouseMove(hour, minIdx) {
 		if (dragging) {
 			dragEndHour = hour;
-			dragEndColumn = columnIdx;
-			if (dragStartHour === dragEndHour && dragStartColumn === dragEndColumn) return;
+			dragEndMin = minIdx;
+			if (dragStartHour === dragEndHour && dragStartMin === dragEndMin) return;
 			if (dragStartHour > dragEndHour) return;
-			cellColors[dragEndHour][0][dragEndColumn].colorFill = 1;
+			cellColors[dragEndHour][0][dragEndMin].colorFill = 1;
+		}
+	}
+
+	// Handle the mouse up event
+	function handleMouseUp(hour, min) {
+		if (dragging) {
+
+			//드래그 시작 시간과 끝 시간이 같고 열 차이가 10분 미만인 경우, 드래그 상태를 초기화하고 함수를 종료.
+			if (dragStartHour === hour && min - dragStartMin < 10) {
+				resetDragState();
+				cellColors[hour][0][min / 10].colorFill = 0;
+				return;
+			}
+
+			// Calculate the duration of the drag in minutes
+			const startTime = {
+				hour: dragStartHour,
+				minute: dragStartMin
+			};
+			const endTime = {
+				hour: min === 50 ? hour + 1 : hour,
+				minute: min === 50 ? 0 : min + 10
+			};
+			
+			addNewRecord(startTime, endTime);
+			resetDragState();
+		}
+	}
+
+	function resetDragState() {
+			dragging = false;
+			dragEndHour = null;
+			dragEndMin = null;
+	}
+
+	function addNewRecord(startTime, endTime) {
+		const startTotalMin = startTime.hour * 60 + startTime.minute;
+		const endTotalMin = endTime.hour * 60 + endTime.minute;
+		// Add a new record if the start time is earlier than the end time
+		if (startTotalMin < endTotalMin) {
+			const newRecord = {
+				start: `${startTime.hour}:${startTime.minute.toString().padStart(2, '0')}`,
+				end: `${endTime.hour}:${endTime.minute.toString().padStart(2, '0')}`,
+				dragged: true // Indicate this record was added by dragging
+			};
+			records = [...records, newRecord];
+			cellColors = getCellColor();
+			console.log('Added new record:', newRecord);
+		} else {
+			cellColors[startTime.hour][0][startTime.minute / 10].colorFill = 0;
 		}
 	}
 </script>
@@ -178,23 +192,26 @@
 					<table class="h-[410px]">
 						<tr>
 							<th></th>
-							{#each columns as column}
-								<th class="!w-[27px] px-1 text-sm">{column + 10}</th>
+							{#each tenMinute as min}
+								<th class="!w-[27px] px-1 text-sm">{min + 10}</th>
 							{/each}
 						</tr>
 						{#each hours.slice(periodIndex * 12, (periodIndex + 1) * 12) as hour}
 							<tr>
 								<th rowspan="2" class="px-1.5">{hour}</th>
-								{#each columns as column, columnIndex}
-									{@const colorFill = cellColors[hour][0][columnIndex].colorFill}
-									{@const record = cellColors[hour][0][columnIndex].record}
+								{#each tenMinute as min, minIndex}
+									{@const colorFill = cellColors[hour][0][minIndex].colorFill}
+									{@const record = cellColors[hour][0][minIndex].record}
 									{#key cellColors}
 										<td
 											class="!m-0 !h-[24px] !w-[24px] !p-0"
 											class:colored={colorFill}
-											on:mousedown={(event) => handleMouseDown(hour, column, event, record)}
-											on:mouseup={() => handleMouseUp(hour, column)}
-											on:mousemove={() => handleMouseMove(hour, columnIndex)}
+											on:mousedown={(event) => handleMouseDown(hour, min, event, record)}
+											on:mouseup={() => handleMouseUp(hour, min)}
+											on:mousemove={() => handleMouseMove(hour, minIndex)}
+											on:touchstart={(event) => handleMouseDown(hour, min, event, record)}
+											on:touchend={() => handleMouseUp(hour, min)}
+											on:touchmove={() => handleMouseMove(hour, minIndex)}
 										>
 											<Popover.Root
 												onOutsideClick={() => (settingVisible = false)}
@@ -211,8 +228,8 @@
 													></Button>
 												</Popover.Trigger>
 												{#if settingVisible && record}
-													<Popover.Content class="translate-y-[0.2rem] w-auto p-0 " >
-														<TimerSetting {record}  />
+													<Popover.Content class="w-auto translate-y-[0.2rem] p-0 ">
+														<TimerSetting {record} />
 													</Popover.Content>
 												{/if}
 											</Popover.Root>
@@ -221,12 +238,23 @@
 								{/each}
 							</tr>
 							<tr>
-								{#each columns as column,columnIndex}
-								{@const record = cellColors[hour][0][columnIndex].record}
-									<td class="!border-0 py-[0.18rem] relative">
-										{#if getProjectColorPosition(hour,column)}
-											<button class="absolute -top-[1.56rem] left-0 w-2 h-6 rounded-r-full  border-r-2 border-zinc-50  " style="background-color:{record?.color}"/>
-										{/if}
+								{#each tenMinute as min, minIndex}
+									{@const record = cellColors[hour][0][minIndex].record}
+									<td class="relative !border-0 py-[0.18rem]">
+										{#key records}
+											{#if getStartRecordPosition(hour, min)}
+												<button
+													class="absolute -top-[1.56rem] left-0 h-6 w-2.5 rounded-r-full border-l-2 border-r-2 border-zinc-50"
+													style="background-color:{record?.color}"
+												/>
+											{/if}
+											{#if getEndRecordPosition(hour, min+10)}
+												<button
+													class="absolute -top-[1.56rem] right-0 h-6 w-2.5 rounded-l-full border-l-2 border-r-2 border-zinc-50 "
+													style="background-color:{record?.color}"
+												/>
+											{/if}
+										{/key}
 									</td>
 								{/each}
 							</tr>
@@ -235,7 +263,9 @@
 				</div>
 			{/each}
 		</div>
-		<div class="absolute left-[42%] top-1 m-auto w-[80px] text-[1.4rem] font-bold font-digital text-violet-950">
+		<div
+			class="font-digital absolute left-[42%] top-1 m-auto w-[80px] text-[1.4rem] font-bold text-violet-950"
+		>
 			{currentTimeDisplay}
 		</div>
 	</div>
