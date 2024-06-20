11508c12c212921ed270820fbd241e0420a24566
int0p <closer.fz@gamil.com>
2024-06-20 15:27:31 +0900

    prerender, url.searchParams

--- src/routes/too/events/+page.server.ts
+++ src/routes/too/events/+page.server.ts
@@ -1,24 +1,15 @@
 import type { Load } from '@sveltejs/kit';
 import { getMonday } from '$lib/utils.js';
 import { CalendarDate } from '@internationalized/date';
-export const load: Load = async ({ fetch, url, prerendering  }) => {
-	let start_date: String | CalendarDate | null = null;
-	let end_date: String | CalendarDate | null = null;
+export const prerender = false;
+export const load: Load = async ({ fetch, url }) => {
+	let start_date: String | CalendarDate | null = url.searchParams.get('start_date');
+	let end_date: String | CalendarDate | null  = url.searchParams.get('end_date');
 
-	// 프리렌더링 시 URL 매개변수를 무시하고 기본값을 설정
-	if (prerendering) {
-		start_date = getMonday(new Date()).subtract({ days: 7 });
+	if (!start_date || !end_date) {
+		start_date = getMonday(new Date()).subtract({days: 7});
 		end_date = start_date.add({ days: 20 });
-	} else {
-		start_date = url.searchParams.get('start_date');
-		end_date = url.searchParams.get('end_date');
-
-		if (!start_date || !end_date) {
-			start_date = getMonday(new Date()).subtract({ days: 7 });
-			end_date = start_date.add({ days: 20 });
-		}
 	}
-	
 	const path = import.meta.env.VITE_SERVER_ENDPOINT + '/api' +`/events?start_date=${start_date}&end_date=${end_date}`;
 
 	try {
