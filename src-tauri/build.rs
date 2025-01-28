1ada8d54cfb7faaf8c1213ca177e06e4e4bbf86b
int0p <closer.fz@gmail.com>
2025-01-28 17:27:19 +0900

    update builder

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,12 +1,21 @@
 fn main() {
     let mut config = prost_build::Config::new();
+    config.type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]");
+    config
+        .extern_path(".google.protobuf.Any", "::prost_wkt_types::Any")
+        .extern_path(".google.protobuf.Timestamp", "::prost_wkt_types::Timestamp")
+        .extern_path(".google.protobuf.Duration", "::prost_wkt_types::Duration")
+        .extern_path(".google.protobuf.Value", "::prost_wkt_types::Value");
     config.out_dir("src/generated"); // 생성된 파일을 특정 디렉토리에 저장
     config
         .compile_protos(
             &[
-                "proto/task_model.proto",
-                "proto/note_model.proto",
-                "proto/chat_model.proto",
+                "proto/chat.proto",
+                "proto/habit.proto",
+                "proto/memo.proto",
+                "proto/task.proto",
+                "proto/note.proto",
+                "proto/weekly.proto",
             ],
             &["proto/"],
         )
