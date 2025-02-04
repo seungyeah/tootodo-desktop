8878ede11471ac1321513dbc705024f31ef41c01
seungyeah <closer.fz@gmail.com>
2025-02-04 22:09:03 +0900

    update db path

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,21 +1,21 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
 mod error;
 mod model;
 mod store;
 
 use model::task::{create_task, delete_task, fetch_tasks, get_task};
+use std::path::PathBuf;
 use store::AppDB;
 use tauri::{generate_context, generate_handler};
+use tauri_api::path::data_dir;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // 데이터베이스 초기화
-    let db_path = format!("{}/rocksdb_data", std::env::var("HOME").unwrap());
-    // println!("{}", &db_path);
-    let app_db = AppDB::new(&db_path)?;
-    // 메모리 주소 출력
-    println!("AppDB address in main: {:p}", &app_db);
+    let mut db_path: PathBuf = data_dir().expect("no data dir");
+    db_path.push("tootodo");
+    db_path.push("rocksdb_data");
+
+    let app_db = AppDB::new(db_path.to_str().expect("invalid db path"))?;
 
     // Tauri 앱 실행
     tauri::Builder::default()
@@ -24,7 +24,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             create_task,
             get_task,
             fetch_tasks,
-            delete_task
+            delete_task,
         ])
         .run(generate_context!())
         .expect("error while running tauri application");
